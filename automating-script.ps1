8d4861bf3f6568d03358e4a99aa52b91c98173a4
Houda SAMAD <houda.samad@um5.ac.ma>
2024-03-24 23:46:53 +0000

    covering the creation of spring-boot projects + maven or gradle projects

--- automating-script.ps1
+++ automating-script.ps1
@@ -14,14 +14,10 @@ function CheckInstallation {
         return "false"
     }
 }
-$isIntalled = CheckInstallation -applicationName "node"
-Write-Host $isIntalled
-
-
 
 #For node projects 
 if($project_type -eq "node"){
-    $isNodeInstalled= CheckInstallation -applicationName "node"
+    $isNodeInstalled = CheckInstallation -applicationName "node"
     $isNpmIntalled = CheckInstallation -applicationName "npm"
     if($isNodeInstalled -eq "false"){
         Write-Host "Node.js is not installed. Please install it before proceeding"
@@ -112,7 +108,6 @@ if($project_type -eq "node"){
                         Write-Host "Visual Studio Code is not installed." -ForegroundColor Red
                         # You can provide an alternative action here if desired
                     }
-    
                 } else {
                     Write-Host "Failed to create repository." -ForegroundColor Red
                 }
@@ -121,6 +116,112 @@ if($project_type -eq "node"){
             }
         }
     }
+}
+elseif ($project_type -eq "spring-boot") {
+    Write-Host "*******************Creating the folder step*******************"
+
+    #Test if the folder already exists 
+    while(Test-Path $PWD/$project_name ){
+        Write-Host "Folder exists, Please provide another name : "  -ForegroundColor Red -NoNewline
+        $project_name = Read-Host 
+    }
+
+    mkdir $project_name
+    cd $project_name
+    Write-Host "*****************Spring Boot Project Metadata*****************"
+
+    #Defining the project parameters
+    $groupId = Read-Host "Please provide the group "
+    $artifactId = Read-Host "Please provide the artifact"
+    $buildId = Read-Host "Do you want a maven or a gradle based project? [Enter 'maven' or 'gradle']"
     
-    
+    if($buildId -eq "maven"){
+        $build ="maven-project"
+    }
+    elseif($buildId -eq "gradle"){
+        $build = "gradle-project"
+    }
+
+    # Construct the URL for Spring Initializr
+    $downloadUrl = "https://start.spring.io/starter.zip?type=$build&groupId=$groupId&artifactId=$artifactId"
+
+    # Download the project zip file
+    Invoke-WebRequest -Uri $downloadUrl -OutFile "$artifactId.zip"
+
+    # Unzip the downloaded file
+    Expand-Archive -Path "$artifactId.zip" -DestinationPath .\
+
+    # Clean up the zip file
+    Remove-Item "$artifactId.zip"
+
+    Write-Host "*******************Creating the repository step*******************"
+
+    # Github username and personal access token
+    $PAT = Read-Host "Please enter your Personal Access Token"
+    $username = Read-Host "Please enter your GitHub username"
+
+    # Base URL for GitHub API
+    $baseUrl = "https://api.github.com"
+
+    # Choose the visibility of the repo (private or public)
+    $visibility = Read-Host "Should the repository be public or private ? (Enter 'public' or 'private')"
+    while($visibility -ne "public" -and $visibility -ne "private"){
+        $visibility = Read-Host "Should the repository be public or private"
+    }
+
+    # Determine if the repository is private
+    $isPrivate = if ($visibility -eq "private") { $true } else { $false }
+
+    # Determine if the repository already exists
+    try {
+        $uri = "$baseUrl/repos/$username/$project_name"
+        $response = Invoke-WebRequest -Uri $uri -Method Get -Headers @{Authorization = "token $PAT"} -ErrorAction Stop
+
+        # Check response status code
+        if ($response.StatusCode -eq 200) {
+            Write-Host "Repository '$project_name' already exists on GitHub."
+        }
+    } catch {
+        # Handle error if repository doesn't exist
+        if ($_.Exception.Response.StatusCode.value__ -eq 404) {
+            # Create a new repository payload
+            $payload = @{
+                name = $project_name
+                auto_init = $true
+                private = $isPrivate
+            } | ConvertTo-Json
+            
+            # Create a new repository using GitHub API
+            $uri = "$baseUrl/user/repos"
+            $response = Invoke-RestMethod -Uri $uri -Method Post -Headers @{Authorization = "token $PAT"} -Body $payload -ContentType "application/json"
+            
+            if ($response) {
+                Write-Host "Repository created successfully." -ForegroundColor Green
+
+                #Creating a local Git repository
+                $isGitInstalled = CheckInstallation -applicationName "git"
+                if($isGitInstalled -eq "true"){
+                    Write-Host "Git is installed. Initializing a new Git Repository." -ForegroundColor Green
+                    git init
+                }
+                else{
+                    Write-Host "Git is not installed." -ForegroundColor Red
+                }
+
+                # Check if 'code' command is available
+                $isVsCodeInstalled = CheckInstallation -applicationName "code"
+                if ($isVsCodeInstalled -eq "true") {
+                    Write-Host "Visual Studio Code is installed. Opening the current directory." -ForegroundColor Green
+                    code .
+                } else {
+                    Write-Host "Visual Studio Code is not installed." -ForegroundColor Red
+                    # You can provide an alternative action here if desired
+                }
+            } else {
+                Write-Host "Failed to create repository." -ForegroundColor Red
+            }
+        } else {
+            Write-Host "An unexpected error occurred while checking repository existence. Status code: $($_.Exception.Response.StatusCode.value__)"
+        }
+    }
 }
